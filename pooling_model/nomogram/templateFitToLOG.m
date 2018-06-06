abc4a5ff80f923b928fbbdaf2ea769f493844178
petteriTeikari <petteri.teikari@gmail.com>
2018-06-06 19:43:59 +0800

    move to a submodule

--- pooling_model/nomogram/templateFitToLOG.m
+++
@@ -1,6 +0,0 @@
-function fit = templateFitToLOG(fit)
-
-    fit.specOut      = log10(fit.specOut);
-    fit.pointsFitted = log10(fit.pointsFitted);
-
-    
--- pooling_model/templates/importSensitivityData.m
+++
@@ -1,7 +0,0 @@
-function spectrum = importSensitivityData(path, fileName, wavelengthCol, sensitivityCol, noOfHeaders, delimiter)
-
-    fileName
-    importedData = importdata(fileName, delimiter, noOfHeaders);
-    x = importedData.data(:,wavelengthCol);
-    y = importedData.data(:,sensitivityCol);
-    spectrum = [x y];    
